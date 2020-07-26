582e418408e4fac4878a3685fa27c7b53dbfe586
davidgeorgebell <bell.dave93@gmail.com>
2020-07-26 20:18:14 +0800

    fetched data from airtable

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,34 @@
-import Nav from '../components/nav'
+import Nav from '../components/nav';
 
-export default function IndexPage() {
+export default function IndexPage({ products }) {
   return (
     <div>
       <Nav />
-      <div className="hero">
-        <h1 className="title">Next.js + Tailwind CSS</h1>
+      <div className='hero'>
+        <h1 className='title'>Coffee club</h1>
       </div>
     </div>
-  )
+  );
+}
+export async function getStaticProps() {
+  const res = await fetch(
+    `https://api.airtable.com/v0/${process.env.TABLE_KEY}/menu`,
+    {
+      method: 'get',
+      headers: {
+        Authorization: `Bearer ${process.env.AIRTABLE_KEY}`,
+      },
+    }
+  );
+  const data = await res.json();
+
+  const products = data.records.map(product => product.fields);
+
+  console.log(products);
+
+  return {
+    props: {
+      products,
+    },
+  };
 }
